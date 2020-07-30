cc7915c3db98e807dbf338333b1152a13b292523
Siken <sikenn@pm.me>
2020-07-30 18:36:47 +0200

    heroku deploy test

--- app.js
+++ app.js
@@ -11,24 +11,24 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 
 const port = process.env.PORT || 3000;
-const dbURI = "mongodb+srv://simon_kenn:l6Ryk1pqLzRFji8O@nodetuts.98u8x.mongodb.net/node-tuts?retryWrites=true&w=majority";
+//const dbURI = "mongodb+srv://simon_kenn:l6Ryk1pqLzRFji8O@nodetuts.98u8x.mongodb.net/node-tuts?retryWrites=true&w=majority";
 
-mongoose.connect(dbURI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-  .then(() => {
-    console.log("Connected to db");
-    app.listen(port, () => {
-      console.log(`App litening on ${port}`);
-    });
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+//mongoose.connect(dbURI, {
+//  useNewUrlParser: true,
+//  useUnifiedTopology: true,
+//})
+//  .then(() => {
+//    console.log("Connected to db");
+//    app.listen(port, () => {
+//      console.log(`App litening on ${port}`);
+//    });
+//  })
+//  .catch((err) => {
+//    console.log(err);
+//  });
 
 app.get("/", cors(), (req, res) => {
   res.send("index");
 });
 
-app.use("/api/shorturl", cors(), routes);
+//app.use("/api/shorturl", cors(), routes);
